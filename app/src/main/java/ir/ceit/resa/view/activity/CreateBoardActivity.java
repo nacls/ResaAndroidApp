e74518dcc97f01539c6c0bca2a59f0a36b25fbb2
Maryam Masoumi <mmasoumi15@gmail.com>
2021-09-27 20:50:09 +0330

    Fix: added toolbar for create board, removed board info icon in info dialog

--- app/src/main/java/ir/ceit/resa/view/activity/CreateBoardActivity.java
+++ app/src/main/java/ir/ceit/resa/view/activity/CreateBoardActivity.java
@@ -1,8 +1,12 @@
 package ir.ceit.resa.view.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
 
 import ir.ceit.resa.R;
 import ir.ceit.resa.contract.CreateBoardContract;
@@ -11,6 +15,8 @@ import ir.ceit.resa.presenter.CreateBoardActivityPresenter;
 public class CreateBoardActivity extends AppCompatActivity implements CreateBoardContract.View {
 
     private CreateBoardActivityPresenter createBoardPresenter;
+    // view
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,25 @@ public class CreateBoardActivity extends AppCompatActivity implements CreateBoar
 
     @Override
     public void setupActivityView() {
+        initializeViewComponents();
+        setupToolbar();
+    }
 
+    private void initializeViewComponents() {
+        toolbar = findViewById(R.id.create_board_toolbar);
+    }
+
+    public void setupToolbar() {
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle(getResources().getString(R.string.create_new_board));
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
